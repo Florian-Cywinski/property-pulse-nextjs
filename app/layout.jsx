c5bdd216704ccf1c6b68e0b7803df8ccedfb8414
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-08-09 14:06:30 +0200

    1 - Next.js Fundamentals & Project Start -> 3 Metadata & Assets

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,10 @@
-// import Navbar from '@/components/Navbar';
-// import Footer from '@/components/Footer';
-// import AuthProvider from '@/components/AuthProvider';
-// import { GlobalProvider } from '@/context/GlobalContext';
-// import { ToastContainer } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
 import '@/assets/styles/globals.css';   // To have Tailwind working // @ points to the root directory
-// import 'photoswipe/dist/photoswipe.css';
 
-// export const metadata = {
-//   title: 'PropertyPulse',
-//   description: 'Find The Perfect Rental Property',
-//   keywords: 'rental, property, real estate',
-// };
+export const metadata = {
+  title: 'PropertyPulse',   // To add a title (tab)
+  description: 'Find The Perfect Rental Property',
+  keywords: 'rental, property, real estate',
+};
 
 const MainLayout = ({ children }) => {    // children is the prop pasted in (page.jsx (homepage))
   return (
@@ -22,18 +15,6 @@ const MainLayout = ({ children }) => {    // children is the prop pasted in (pag
         </main>
       </body>
     </html>
-    // <AuthProvider>
-    //   <GlobalProvider>
-    //     <html lang='en'>
-    //       <body>
-    //         <Navbar />
-    //         <main>{children}</main>
-    //         <Footer />
-    //         <ToastContainer />
-    //       </body>
-    //     </html>
-    //   </GlobalProvider>
-    // </AuthProvider>
   );
 };
 
--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,8 @@
-// import Hero from '../components/Hero';
-// import InfoBoxes from '@/components/InfoBoxes';
-// import HomeProperties from '@/components/HomeProperties';
-// import FeaturedProperties from '@/components/FeaturedProperties';
-
 const HomePage = () => {
   return (
     <div className="text-2xl">
       Home Page
     </div>
-    // <>
-    //   <Hero />
-    //   <InfoBoxes />
-    //   <FeaturedProperties />
-    //   <HomeProperties />
-    // </>
   );
 };
 export default HomePage;
