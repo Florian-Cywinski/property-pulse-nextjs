42e6b2cf491a64371304bf23444f5c667e196385
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-08-10 02:45:36 +0200

    1 - Next.js Fundamentals & Project Start -> 5 Client Component & Navigation Hooks

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,4 +1,11 @@
-const PropertyPage = () => {
-  return <div>Single Property page</div>
+const PropertyPage = ({ params }) => {  // params to get the id (or whatever is required)
+
+  return (
+    <>
+      <div>
+        Single Property page { params.id }  {/* To show 'Single Property page 9' on http://127.0.0.1:3000/properties/9?name=brad */}
+      </div>
+    </>
+  )
 };
 export default PropertyPage;
