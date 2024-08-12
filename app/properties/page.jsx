930ca66c358dfe68792bb1a655dc9d296b6fb9cc
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-08-12 13:24:32 +0200

    1 - Next.js Fundamentals & Project Start -> 10 Properties Page & PropertyCard Component

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,6 +1,27 @@
-const PropertiesPage = async ({ searchParams: { pageSize = 9, page = 1 } }) => {
+import properties from '@/properties.json'
+import PropertyCard from '@/components/PropertyCard';
 
-  return <div>Properties page</div>
+const PropertiesPage = () => {
+  // console.log(properties);
+  
+  return (
+    <>
+      <section className='px-4 py-6'>
+        <div className='container-xl lg:container m-auto px-4 py-6'>
+          <h1 className='text-2xl mb-4'>Browse Properties</h1>
+          {properties.length === 0 ? (
+            <p>No properties found</p>
+          ) : (
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+              {properties.map((property, index) => (
+                <PropertyCard property={property} key={index} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
+  );
 
 };
 
