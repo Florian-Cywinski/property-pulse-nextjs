880c0a61904840f02ea79252074ab8ae7531a2b8
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-08-22 13:50:08 +0200

    6 - Map, Bookmarking & Sharing -> 2 - Mapbox Map Display

--- components/PropertyMap.jsx
+++ components/PropertyMap.jsx
@@ -1,6 +1,11 @@
 'use client';
 import { useEffect, useState } from 'react';
 import { setDefaults, fromAddress } from 'react-geocode';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import Map, { Marker } from 'react-map-gl';
+import Image from 'next/image';
+import pin from '@/assets/images/pin.svg';
+import Spinner from './Spinner';
 
 const PropertyMap = ({ property }) => {
   const [lat, setLat] = useState(null);
@@ -43,6 +48,8 @@ const PropertyMap = ({ property }) => {
           latitude: lat,
           longitude: lng,
         });
+        console.log(lat, lng);
+        
       } catch (error) {
         console.log(error);
         setGeocodeError(true);
@@ -54,12 +61,31 @@ const PropertyMap = ({ property }) => {
     fetchCoords();
   }, []);
 
-  if (loading) return <h3>Loading...</h3>;
+  if (loading) return <Spinner loading={loading} />;
 
   if (geocodeError)
     return <div className='text-xl'>No location data found</div>;
 
-  return <div>Map</div>;
+  // return <div>Map</div>;
+  return (
+    !loading && ( // https://github.com/visgl/react-map-gl
+      <Map
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+        mapLib={import('mapbox-gl')}
+        initialViewState={{
+          longitude: lng,
+          latitude: lat,
+          zoom: 15,
+        }}
+        style={{ width: '100%', height: 500 }}
+        mapStyle='mapbox://styles/mapbox/streets-v9'
+      >
+        <Marker longitude={lng} latitude={lat} anchor='bottom'>
+          <Image src={pin} alt='location' width={40} height={40} />
+        </Marker>
+      </Map>
+    )
+  );
 };
 
 export default PropertyMap;
